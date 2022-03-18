ff42e07270911ab81c23562ead1f7800c12b3ca1
Adam Thahir <adamthahir@gmail.com>
2022-03-18 19:46:46 +0300

    basic functions

---
+++ WordleAnalyzer.py
@@ -0,0 +1,158 @@
+import cv2
+import numpy as np
+import math
+import datetime
+
+
+class WordleAnalyzer:
+
+    def __init__ (self, todate=None):
+        if todate == None:
+            todate = datetime.datetime.now().date().isoformat()
+        self.TODATE = todate
+
+    def OpenFile (self):
+        self.IMAGE = cv2.imread(f"{self.TODATE}.png")
+
+    def CropImage (self, img):
+        center_x, center_y = img.shape[1] / 2, img.shape[0] / 2
+
+        width_scaled, height_scaled = img.shape[1] * 0.5, img.shape[0] * 0.85
+        left_x, right_x = center_x - width_scaled / 2, center_x + width_scaled / 2
+        top_y, bottom_y = center_y - height_scaled / 2, center_y + (center_y - height_scaled / 2.8)
+    
+        self.CROPPED = img[int(top_y):int(bottom_y), int(left_x):int(right_x)]
+
+        return self.CROPPED
+
+    def FindColor(self, img, lower, upper):
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv, lower, upper)
+
+        return mask
+
+    def FindYellows (self, img):
+        lower_bound = np.array([25,50,50])   
+        upper_bound = np.array([32,255,255])
+        
+        return self.FindColor(img, lower_bound, upper_bound)
+
+    def FindGreens (self, img):
+        lower_bound = np.array([36, 50, 70])   
+        upper_bound = np.array([89, 255, 255])
+        
+        return self.FindColor(img, lower_bound, upper_bound)
+
+    def FindCenters (self, img):
+        # Finds centers of non matches (non matches are sent as img)
+        contours, hierarchy = cv2.findContours(img, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+        CenterPoints = []
+        for contour in contours:
+            area = cv2.contourArea(contour)
+
+            epsilon = .1 * cv2.arcLength(contour, True)
+            approx = cv2.approxPolyDP(contour, epsilon, True)
+
+            if not approx.shape[0] == 4:
+                continue
+            else:
+                lenA = max(max(abs(approx[0] - approx[1])))
+                lenB = max(max(abs(approx[1] - approx[2])))
+                lenC = max(max(abs(approx[2] - approx[3])))
+
+                if not (lenA == lenB and lenB == lenC):
+                    continue
+            
+            # X1, Y1 = approx[0]  
+            minX, maxX = None, None
+            minY, maxY = None, None
+
+            for point in approx:
+                x = point[0][0]
+                y = point[0][1]
+
+                if maxX == None or x > maxX:
+                    maxX = x
+                
+                if minX == None or x < minX:
+                    minX = x
+
+                if maxY == None or y > maxY:
+                    maxY = y
+                
+                if minY == None or y < minY:
+                    minY = y
+            
+            centerX = (maxX - minX) + minX
+            centerY = (maxY - minY) + minY
+            CenterPoints.append( (centerX, centerY) )
+
+        return CenterPoints
+    
+    def MakeBoard (self):
+        board = cv2.imread("base.png")
+        Cropped = self.CropImage (board)
+
+        cv2.imwrite(f"board_2.png", Cropped)
+
+
+    def RunAttempt (self, initial=False):
+        Original = cv2.imread(f"{self.TODATE}.png")
+        
+        if initial:
+            board = cv2.imread("board_2.png")
+        else:
+            board = cv2.imread(f"{self.TODATE}_PREV.png")
+
+        Cropped = self.CropImage (Original)
+
+        print (f'Sizes: [{Cropped.shape}] | [{board.shape}] || {Original.shape}')
+        attemptDifference = cv2.absdiff(Cropped, board)
+
+        grayDifference = cv2.cvtColor(attemptDifference, cv2.COLOR_BGR2GRAY)
+        binaryDifference = cv2.threshold(grayDifference, 5, 255, cv2.THRESH_BINARY)[1]
+
+        YellowMask = self.FindYellows(attemptDifference)
+        GreenMask = self.FindGreens(attemptDifference)
+
+
+        CentersOfAll = self.FindCenters(binaryDifference)
+
+        assert len(CentersOfAll) != 0, 'No centers found; to start from.'
+
+        NonYellows  = cv2.absdiff(binaryDifference, YellowMask)
+        NonGreens   = cv2.absdiff(binaryDifference, GreenMask)
+
+
+        CentersOfNonYellows = self.FindCenters(NonYellows)
+        CentersOfNonGreens = self.FindCenters(NonGreens)
+
+        TruePositions = [val[0] for val in CentersOfAll]
+        TruePositions.sort()
+
+        YellowIndex = []
+        GreenIndex = []
+
+        if not len(CentersOfAll) == len(CentersOfNonYellows):
+            for i, center in enumerate(CentersOfAll):
+                if not center in CentersOfNonYellows:
+                    YellowIndex.append(TruePositions.index(center[0]))
+        
+        if not len(CentersOfAll) == len(CentersOfNonGreens):
+            for i, center in enumerate(CentersOfAll):
+                if not center in CentersOfNonGreens:
+                    GreenIndex.append(TruePositions.index(center[0]))
+
+        # print (f"Yellows: {YellowIndex}")      
+        # print (f"Greens: {GreenIndex}")    
+          
+        # print (f"All: {CentersOfAll}")      
+        # print (f"NonYellows: {CentersOfNonYellows}")      
+        # print (f"NonGreens: {CentersOfNonGreens}")      
+
+        cv2.imwrite(f"{self.TODATE}_PREV.png", Cropped)
+
+        return YellowIndex, GreenIndex
+
+
---
+++ one.py
@@ -0,0 +1,212 @@
+import datetime
+import time
+
+from WordleAnalyzer import WordleAnalyzer
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium.webdriver.common.by import By
+
+def GetShadowRootObject (driver, tagname):
+    query = f"return document.getElementsByTagName ('{tagname}')[0].shadowRoot"
+    return driver.execute_script (query)
+
+def FindScriptFile (driver):
+    page = driver.find_elements(By.TAG_NAME, 'script')
+    for element in page:
+        link = str(element.get_attribute('src'))
+        if '/games/wordle/main' in link and '.js' == link[-3:]:
+            return link
+
+def GotoTab (driver, tabIndex):
+    assert len (driver.window_handles) > tabIndex
+    driver.switch_to.window(driver.window_handles[tabIndex])
+
+def OpenLinkNewTab (driver, link):
+    driver.execute_script(f"window.open('{link}','_blank')")
+
+def AddWordList (wordLists, wordListString):
+    wordListString = wordListString.replace("[","").replace("]", "").replace("\"", "")
+    wordList = wordListString.split(",")
+
+    for word in wordList:
+        word = word.strip()
+
+        if word not in wordLists and len (word) == 5:
+            wordLists.append(word)
+
+    return wordLists
+
+def ExtractWordList (driver):
+    GotoTab (driver, 1)
+
+    time.sleep(5)
+
+    fileContent = driver.find_elements(by=By.TAG_NAME, value="body")[0].text
+
+    wordLists = []
+
+    # Get List A
+    startIndex = fileContent.find("var Ma=")
+    subContent = fileContent[startIndex:]
+
+    # print (subContent)
+
+    restartIndex = subContent.find("[")
+    endIndex = subContent.find("]")
+
+    wordListString = subContent[restartIndex:endIndex]
+    wordLists = AddWordList (wordLists, wordListString)
+
+    # Get List B
+    startIndex = subContent.find("[")
+    subContent = fileContent[startIndex:]
+
+    restartIndex = subContent.find("[")
+    endIndex = subContent.find("]")
+
+    wordListString = subContent[restartIndex:endIndex]
+    wordLists = AddWordList (wordLists, wordListString)
+
+    # GotoTab (driver, 0)
+
+    return wordLists
+
+def GuessWord (driver, word):
+    # For now, always assume word is valid.
+    GotoTab (driver, 0)
+    element = driver.find_elements(by=By.TAG_NAME, value="body")[0]
+
+    assert len (word) == 5
+    print (word)
+    element.send_keys(word)
+    element.send_keys(Keys.RETURN)
+
+    todate = datetime.datetime.now().date().isoformat()
+
+    time.sleep(3)
+    state = driver.save_screenshot(f'{todate}.png')
+    print (f'Screenshot saved: {state}')
+
+def SaveBase (driver):
+    # For now, always assume word is valid.
+    GotoTab (driver, 0)
+    element = driver.find_elements(by=By.TAG_NAME, value="body")[0]
+
+    time.sleep(3)
+    state = driver.save_screenshot(f'base.png')
+    print (f'Base saved: {state}')
+
+def FindWord (attempted, ignoreLetters, knownLetters, lettersWithPlaces):
+    for i, word in enumerate(wordList):
+        ignoreWord = False
+
+        if 'saute' in word:
+            print (i, word)
+
+        if len(ignoreLetters) > 0:
+            for char in word:
+                if char in ignoreLetters:
+                    ignoreWord = True
+                    break
+
+        if len(lettersWithPlaces) > 0:
+            for char, position in lettersWithPlaces:
+                if not (word[position] == char):
+                    if 'saute' in word:
+                        print (f"char: {char} | {position} || {word}")
+                        print (f"{word[position]} || {char}")
+                    ignoreWord = True
+                    break
+
+        if len(knownLetters) > 0:
+            for char in knownLetters:
+                if not char in word:
+                    ignoreWord = True
+                    if 'saute' in word:
+                        print (f"char: {char} || {word}")
+                    break
+        
+        if ignoreWord:
+            continue
+        
+        return word
+
+if __name__ == "__main__":
+    binary = FirefoxBinary('C:\\Users\\adamthahir\\Documents\\geckodriver.exe')
+    driver = webdriver.Firefox(executable_path='C:\\Users\\adamthahir\\Documents\\geckodriver.exe')
+
+    site = 'https://www.nytimes.com/games/wordle/index.html'
+    driver.get(site)
+    time.sleep(2)
+
+    driver.find_elements(by=By.TAG_NAME, value="body")[0].click()
+
+    scriptLink = FindScriptFile(driver)
+    OpenLinkNewTab(driver, scriptLink)
+
+    wordList = ExtractWordList (driver)
+    print (f'length: {len(wordList)}')
+
+    SaveBase(driver)
+
+    todate = datetime.datetime.now().date().isoformat()
+    analyzer = WordleAnalyzer(todate)
+
+    analyzer.MakeBoard()
+
+    attempts = 0
+
+    attempted = []
+    ignoreLetters = []
+    knownLetters = []
+    lettersWithPlaces = []
+
+    print (f'Saute: {"saute" in wordList}')
+    
+    while True:
+        time.sleep(5)
+
+        word = FindWord(attempted, ignoreLetters, knownLetters, lettersWithPlaces)
+        
+        GuessWord (driver, word)
+        attempts += 1
+
+        yellows, greens = analyzer.RunAttempt(attempts==1)
+
+        yellowLetters = [word[i] for i in yellows]
+        greenLetters = [word[i] for i in greens]
+
+        if len(yellows) > 0:
+            for yellow in yellows:
+                knownLetters.append(word[yellow])
+                attempted.append(word[yellow])
+
+        if len(greens) > 0:
+            for green in greens:
+                attempted.append(word[green])
+                lettersWithPlaces.append( (word[green], green) )
+
+        for i, char in enumerate(word):
+            if not char in yellowLetters and not char in greenLetters:
+                ignoreLetters.append(word[i])
+
+        print (f'Word: {word}')
+        print (f'Yellows: {yellows}')
+        print (f'Greens: {greens}')
+        print (f'ignore: {ignoreLetters}')
+
+        if attempts >= 6 or len(greens) >= 5:
+            print (f'Exit. Attempts: {attempts} || Solved: {len(greens)}')
+            break
+
+
+# assert "Python" in driver.title
+
+# elem = driver.find_element_by_name("q")
+# elem.clear()
+# elem.send_keys("pycon")
+# elem.send_keys(Keys.RETURN)
+# assert "No results found." not in driver.page_source
+# driver.close()
